2f79033343fc9b56945637dafbce8d9ed66277d4
koarz <3577087577@qq.com>
2024-10-21 22:17:00 +0800

    use xmake

---
+++ xmake.lua
@@ -0,0 +1,12 @@
+add_rules("mode.debug", "mode.release")
+add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
+set_languages("c++23")
+set_project("ZeitgeistDB")
+set_version("0.1")
+add_includedirs("src/include")
+add_requires("linenoise")
+
+target("ZeitgeistDB")
+    set_kind("binary")
+    add_files("src/main.cpp")
+    add_packages("linenoise")
