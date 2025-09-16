2cacc87c1c455da524aad1ef3c82d42f17685051
koarz <3577087577@qq.com>
2025-09-16 06:49:58 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -5,12 +5,16 @@ if [ "${param}" = "help" ]; then
     echo "bpm     start buffer pool manager bench"
     echo "file    start file control bench"
     echo "test    start all tests"
+elif [ "${param}" = "db" ]; then
+    xmake build ZeitKert
+    xmake run ZeitKert
 elif [ "${param}" = "bpm" ]; then
     xmake build bpm-bench
     xmake run bpm-bench
 elif [ "${param}" = "file" ]; then
-    clang++ -std=c++23 -g -O3 ./benchmark/file-control-bench.cpp -o ./build/linux/x86_64/debug/file-control-bench
-    ./build/linux/x86_64/debug/file-control-bench
+    mkdir build
+    clang++ -std=c++23 -g -O3 ./benchmark/file-control-bench.cpp -o ./build/file-control-bench
+    ./build/file-control-bench
 elif [ "${param}" = "test" ]; then
     xmake build tests
     xmake run tests
