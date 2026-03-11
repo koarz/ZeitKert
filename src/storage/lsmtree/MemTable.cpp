925a24a7b324f1100afdf252b34e6f646afed6f2
koarz <3577087577@qq.com>
2026-03-11 14:31:56 +0800

    删除空文件

--- src/storage/lsmtree/MemTable.cpp
+++
@@ -1,8 +0,0 @@
-#include "storage/lsmtree/MemTable.hpp"
-
-// MemTable 现在是 VectorizedMemTable 的薄封装
-// 所有实现都在 VectorizedMemTable.hpp 的头文件中
-
-namespace DB {
-// 空实现文件 - 保留以维持构建系统兼容性
-} // namespace DB
